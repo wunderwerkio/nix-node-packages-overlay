997fa95b80fdbf2d30e55de93feaa626ea388ccd
chfoidl <chfoidl@users.noreply.github.com>
2024-01-03 01:36:02 +0000

    chore: update pnpm to 8.14.0

--- overlays/pnpm.nix
+++ overlays/pnpm.nix
@@ -1,6 +1,6 @@
 final: prev: let
-  version = "8.12.1";
-  sha256 = "sha256-VT5OsOKiyavLQZsyYr3HruiuPELiMBoYB9RFdXhhYMk=";
+  version = "8.14.0";
+  sha256 = "sha256-nOv2Gr2D9oF3spSE2nLal1E5DqrUbfwwctJmv7sbp78=";
   lib = import ../lib {pkgs = prev;};
 in
   lib.overlayNodePackages {
